d04b7a9d00003b32bd3c45faef7c37848a25ed7d
Lesley Cordero <lc2958@columbia.edu>
2015-06-16 10:45:15 -0400

    Create query.py
    Allows user to search for item

---
+++ query.py
@@ -0,0 +1,44 @@
+import re
+import sys
+
+class PyMongoQuery(dict):
+
+    def __and__(self, other):
+	return PyMongoQuery(list(self.items()) + list(other.items()))
+
+    def __or__(self, other):
+	return PyMongoQuery({'$or': [self, other]})
+
+
+class k(object):
+	def __init__(self, key):
+		self.key = key
+
+	def __eq__(self, value):
+		return PyMongoQuery({self.key: value})
+
+	def __neq__(self, value):
+		return PyMongoQuery({self.key: {'$ne': value}})
+
+	def __gt__(self, value):
+		return PyMongoQuery({self.key: {'$gt': value}})
+
+	def __gte__(self, value):
+		return PyMongoQuery({self.key: {'$gte': value}})
+
+	def __lt__(self, value):
+		return PyMongoQuery({self.key: {'$lt': value}})
+
+	def __lte__(self, value):
+		return PyMongoQuery({self.key: {'$lte': value}})
+
+	def starts_with(self, string):
+		return PyMongoQuery({self.key: {'$regex': '^' + re.escape(string)}})
+
+	def ends_with(self, string):
+		return PyMongoQuery({self.key: {'$regex': re.escape(string) + '$'}})   
+    
+ 
+if __name__ == "__main__":
+    search = sys.argv[1]
+    
